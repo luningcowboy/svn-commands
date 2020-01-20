3501b8e2b2eaeda8c7e4d50daaf39082376ba682
luningcowboy <luningcowboy@gmail.com>
2020-01-20 15:11:16 +0800

    add add_all bash

---
+++ svn_add_all.sh
@@ -0,0 +1,11 @@
+#bin/bash
+PATH=$1
+cd $PATH
+ret=`svn stat`
+echo "修改的文件:"
+echo $ret
+files=$(svn st | grep ? | sed 's/\?//g' | sed 's/ //g')
+echo "需要添加的文件:"
+for i in $files; do
+	svn add $i
+done
